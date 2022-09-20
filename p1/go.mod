377adb9b88ecb1163901c5a0d040ff6db748bcc3
LEAFERx <leaferx@outlook.com>
2022-09-20 15:34:41 -0700

    test

--- p1/go.mod
+++ p1/go.mod
@@ -1,3 +1,5 @@
 module github.com/LEAFERx/go-mod-test/p1
 
 go 1.19
+
+require github.com/LEAFERx/go-mod-test v0.0.0-20220920222859-88088767b28d
--- p1/lib.go
+++ p1/lib.go
@@ -1,5 +1,9 @@
 package p1
 
+import (
+	x "github.com/LEAFERx/go-mod-test"
+)
+
 func LibFn1(a int) int {
-	return a + 2
+	return x.LibFn(a + 2)
 }
